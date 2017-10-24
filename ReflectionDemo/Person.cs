5af9ba8f4aafd300aa4669fcd1e90d6ad2edbcd6
Tommy <tommy.h.li@newegg.com>
2017-10-24 13:51:13 +0800

    Add a demo project for Reflection

---
+++ ReflectionDemo/Person.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    public class Person
+    {
+        private int age = 0;
+        private string name = String.Empty;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public void Load()
+        {
+            if (File.Exists("settings.dat"))
+            {
+                Type type = this.GetType();
+
+                string propertyName, value;
+                string[] temp;
+                char[] splitChars = new char[] { '|'};
+                PropertyInfo propertyInfo;
+
+                string[] settings = File.ReadAllLines("settings.dat");
+                foreach (string s in settings)
+                {
+                    temp = s.Split(splitChars);
+                    if (temp.Length == 2)
+                    {
+                        propertyName = temp[0];
+                        value = temp[1];
+                        propertyInfo = type.GetProperty(propertyName);
+                        if (propertyInfo != null)
+                            this.SetProperty(propertyInfo, value);
+
+                    }
+                }
+            }
+        }
+
+        public void SetProperty(PropertyInfo propertyInfo, object value)
+        {
+            switch (propertyInfo.PropertyType.Name)
+            {
+                case "Int32":
+                    propertyInfo.SetValue(this, Convert.ToInt32(value), null);
+                    break;
+                case "String":
+                    propertyInfo.SetValue(this, value.ToString(), null);
+                    break;
+            }
+        }
+
+        public void Save()
+        {
+            Type type = this.GetType();
+            PropertyInfo[] properties = type.GetProperties();
+            TextWriter tw = new StreamWriter("settings.dat");
+            foreach (PropertyInfo propertyInfo in properties)
+            {
+                tw.WriteLine(propertyInfo.Name + "|" + propertyInfo.GetValue(this, null));
+            }
+            tw.Close();
+        }
+
+    }
+}
---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Load();
+            if ((person.Age > 0) && (person.Name != String.Empty))
+            {
+                Console.WriteLine("Hi " + person.Name + " - you are " + person.Age + " years old!");
+            }
+            else
+            {
+                Console.WriteLine("I don't seem to know much about you. Please enter the following information:");
+                Type type = typeof(Person);
+                PropertyInfo[] properties = type.GetProperties();
+                foreach (PropertyInfo propertyInfo in properties)
+                {
+                    Console.WriteLine(propertyInfo.Name + ":");
+                    person.SetProperty(propertyInfo, Console.ReadLine());
+                }
+                person.Save();
+                Console.WriteLine("Thank you! I have saved your information for next time.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
